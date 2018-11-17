df34142ddb762a7f30c03db55cab321a63dd1f34
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-11-17 17:04:16 -0200

    added action to overViewController

--- HackaPortoCred/SingupOverviewViewController.swift
+++ HackaPortoCred/SingupOverviewViewController.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class SingupOverviewViewController: UIViewController {
 
+    var person: Person?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func validateInfo(_ sender: UIBarButtonItem) {
+        guard let person = person else { return }
+        Biometrics.shared.createUser(person) { (success, error) in
+            
+        }
     }
-    */
-
 }
