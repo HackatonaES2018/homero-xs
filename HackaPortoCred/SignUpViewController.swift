5d54870f0898d41b42b68478aaeb5499933f6856
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-11-17 18:38:03 -0200

    add pickerDate

--- HackaPortoCred/SignUpViewController.swift
+++ HackaPortoCred/SignUpViewController.swift
@@ -18,10 +18,14 @@ class SignUpViewController: UIViewController {
                                 birthDate: Date(),
                                 cpf: "",
                                 phoneNumber: "")
-
+    let datePicker = UIDatePicker()
+    var date: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setTableView()
+        datePicker.addTarget(self, action: #selector(getDate), for: .valueChanged)
+        datePicker.maximumDate = Date()
     }
     
 
@@ -32,6 +36,13 @@ class SignUpViewController: UIViewController {
         destination.person = person
     }
 
+    @objc func getDate(sender: UIDatePicker) {
+        date = sender.date
+        guard let cell = tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? UISignUpTableViewCell else {
+            return
+        }
+        cell.textField.text = date?.description
+    }
 }
 
 extension SignUpViewController: UITableViewDelegate, UITableViewDataSource {
@@ -51,26 +62,75 @@ extension SignUpViewController: UITableViewDelegate, UITableViewDataSource {
             return UITableViewCell()
         }
         cell.selectionStyle = .none
+        cell.textField.tag = indexPath.row
+        cell.textField.delegate = self
         if indexPath.row == 0 {
             cell.title = "Nome"
             cell.textField.placeholder = "John Doe"
-        } else if indexPath.row == 2 {
-            cell.title = "Data de nscimento"
+            cell.textField.text = person.name
+        } else if indexPath.row == 1 {
+            cell.title = "Data de nacimento"
             cell.textField.placeholder = "XX/XX/XXXX"
+            cell.textField.text = date?.description
         } else if indexPath.row == 2 {
             cell.title = "E-Mail"
-            cell.textField.placeholder = "E-Mail"
+            cell.textField.placeholder = "john.doe@test.com"
+            cell.textField.text = person.email
         } else if indexPath.row == 3 {
             cell.title = "Celular"
-            cell.textField.placeholder = "john.doe@test.com"
+            cell.textField.placeholder = "(00)00000-0000"
+            cell.textField.text = person.phoneNumber
         } else if indexPath.row == 4 {
             cell.title = "CPF"
             cell.textField.placeholder = "000.000.000-00"
+            cell.textField.text = person.cpf
         }
         return cell;
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return tableView.frame.height
+    }
+}
+
+extension SignUpViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        switch textField.tag {
+        case 0:
+            textField.keyboardType = .namePhonePad
+            textField.textContentType = .name
+        case 1:
+            textField.inputView = datePicker
+        case 2:
+            textField.keyboardType = .emailAddress
+            textField.textContentType = .emailAddress
+        case 3:
+            textField.keyboardType = .numberPad
+            textField.textContentType = .telephoneNumber
+        case 4:
+            textField.keyboardType = .numberPad
+        default:
+            break
+        }
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let text = textField.text ?? ""
+        switch textField.tag {
+        case 0:
+            person.name = text
+        case 1:
+            break
+        case 2:
+            person.email = text
+        case 3:
+            person.phoneNumber = text
+        case 4:
+            person.cpf = text
+        default:
+            break
+        }
     }
 }
